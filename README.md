cbd40d8b83ff02d208a1b2407e8ad8601e042d21
osefatul <sefaullahomar2@gmail.com>
2021-08-13 08:17:30 -0700

    sidebar top is done

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # WhatsApp clone:
 
-I built this clone using Javasript, react, redux, firestor-firebase, authentication-firebase, storage-firebase...
+I built this clone using Javasript, react, next, redux, firestor-firebase, authentication-firebase, storage-firebase...
 
 - We used nextJs. we will introduce and explore nextJs at the same time.
-	*Pages directory is for the rout pages. dont make any file there. 
-- we will be using styled component for css styling.
-	* npm install --save styled-components
+- Pages directory - is for the rout pages. dont create any component file there. 
+- components directory will be here for components files.
 
- 
+
+- we will be using styled and Material UI components for css styling.
+	* npm install --save styled-components
+	* npm install @material-ui/icons
+
+
+- Create Sidebar
--- componoents/SIdebar.js
+++ componoents/SIdebar.js
@@ -1,7 +1,79 @@
 import React from "react";
+import styled from "styled-components";
+import { Avatar, Button, IconButton } from "@material-ui/core";
+import { Chat, MoreVert } from "@material-ui/icons";
+import SearchIcon from "@material-ui/icons/search";
 
-function SIdebar() {
-  return <div></div>;
+function Sidebar() {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+
+        <IconContainer>
+          <IconButton>
+            <Chat />
+          </IconButton>
+
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </IconContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+
+      <SidebarButton>Start a New Chat</SidebarButton>
+
+      {/* List of Chats  */}
+    </Container>
+  );
 }
 
-export default SIdebar;
+export default Sidebar;
+
+const Container = styled.div``;
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1; //will be on the top of each everyone if we have overflow
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+const IconContainer = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+const SidebarButton = styled(Button)`
+  width: 100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
