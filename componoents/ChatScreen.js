4251356cca08797bf8b8412d61b323b6c3870f69
osefatul <sefaullahomar2@gmail.com>
2021-09-12 21:44:46 -0700

    the chat screen header is done

--- componoents/ChatScreen.js
+++ componoents/ChatScreen.js
@@ -1,12 +1,66 @@
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import { auth } from "../firebase";
+import { useRouter } from "next/router";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert } from "@material-ui/icons";
 
 function ChatScreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
   return (
     <Container>
-      <h1>This is Chat Screen</h1>
+      <Header>
+        <Avatar />
+
+        <HeaderInformations>
+          <h3>Rec Email</h3>
+          <p>Last Seen...</p>
+        </HeaderInformations>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+
+      <MessageContainer>
+        {/* function for showing messages */}
+        <EndOfMessage />
+      </MessageContainer>
     </Container>
   );
 }
 
 export default ChatScreen;
 const Container = styled.div``;
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+const HeaderInformations = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+const HeaderIcons = styled.div``;
+const MessageContainer = styled.div``;
+const EndOfMessage = styled.div``;
